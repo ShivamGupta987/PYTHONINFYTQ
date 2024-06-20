5188a73ea4a99325d9de16589268341c7cd4e1b5
nikhil <sg804595@gmail.com>
2024-06-20 10:20:55 +0530

    loops solved

--- Loops.py
+++ Loops.py
@@ -1,33 +1,33 @@
-# while True:
-#     # Prompt the user to enter the color of the signal
-#     signal_color = input("Enter the color of the traffic signal (RED/YELLOW/GREEN) or 'quit' to exit: ").upper()
+while True:
+    # Prompt the user to enter the color of the signal
+    signal_color = input("Enter the color of the traffic signal (RED/YELLOW/GREEN) or 'quit' to exit: ").upper()
     
-#     # Check if the user wants to quit
-#     if signal_color == "QUIT":
-#         print("Exiting the program.")
-#         break
+    # Check if the user wants to quit
+    if signal_color == "QUIT":
+        print("Exiting the program.")
+        break
     
-#     # Display the appropriate message based on the input signal color
-#     if signal_color == "RED":
-#         print("Stop")
-#     elif signal_color == "YELLOW":
-#         print("Stay")
-#     elif signal_color == "GREEN":
-#         print("Go")
-#     else:
-#         print("Invalid signal color")
+    # Display the appropriate message based on the input signal color
+    if signal_color == "RED":
+        print("Stop")
+    elif signal_color == "YELLOW":
+        print("Stay")
+    elif signal_color == "GREEN":
+        print("Go")
+    else:
+        print("Invalid signal color")
 
 
-# # Pre-specified numbers
-# a = 10
-# b = 20
-# c = 15
+# Pre-specified numbers
+a = 10
+b = 20
+c = 15
 
-# # Find the largest number using ternary operators
-# largest = a if (a > b and a > c) else (b if b > c else c)
+# Find the largest number using ternary operators
+largest = a if (a > b and a > c) else (b if b > c else c)
 
-# # Display the largest number
-# print("The largest number is:", largest)
+# Display the largest number
+print("The largest number is:", largest)
 
 # Input: Whole number from the user
 number = int(input("Enter a whole number to find its factors: "))
