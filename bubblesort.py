26d4d55d0289030bb691cb0c2651b9b2d3013dac
nikhil <sg804595@gmail.com>
2024-06-21 14:03:18 +0530

    enhanced also added

--- bubblesort.py
+++ bubblesort.py
@@ -13,3 +13,25 @@ def bubble_sort(Arr):
 Arr = [1,5,6,2,3]
 sorted_Arr = bubble_sort(Arr)
 print(sorted_Arr)
+
+
+
+
+def bubble_sort_enhanced(Arr):
+    n = len(Arr)
+    
+    for i in range(n-1):
+        is_sorted = True
+        for j in range(n-i-1):
+            is_sorted = False
+            if Arr[j] > Arr[j+1]:
+                Arr[j] , Arr[j+1]= Arr[j+1] , Arr[j]
+                
+            if is_sorted == True :
+                break
+                
+    return Arr
+                
+Arr = [1,5,6,2,3]
+sorted_Arr = bubble_sort_enhanced(Arr)
+print(sorted_Arr)
