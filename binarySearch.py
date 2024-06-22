e3470325623a9a8e712db71da37a7ffb91eabda0
nikhil <sg804595@gmail.com>
2024-06-22 14:01:04 +0530

    solved problem of binary search lower bound

--- binarySearch.py
+++ binarySearch.py
@@ -23,3 +23,28 @@ def binary_seacrh(arr,target):
 arr = [1,2,4,5,6,9,12,18,19]
 
 print(binary_seacrh(arr,12))
+
+
+# lower bound problem solved leetcode
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        low = 0 
+        high = len(nums) - 1 
+
+        while low <= high :
+            mid = ((low + high) // 2)
+
+            if nums[mid] == target:
+                return mid 
+
+            elif nums[mid] <= target:
+                low = mid + 1 
+                
+            else :
+                 high = mid - 1
+                 
+
+        return low
+        
+        
