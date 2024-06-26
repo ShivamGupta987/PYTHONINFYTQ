cb6bb786bf31e26cec31a0d067d5c519da0d341d
nikhil <sg804595@gmail.com>
2024-06-26 12:39:02 +0530

    fibo

--- recursion.py
+++ recursion.py
@@ -5,3 +5,10 @@ def fctorial(n):
     else:
         return n * fctorial(n-1)
 print(fctorial(5))
+def fibo(n):
+    # base cond 
+    if n<=1:
+        return n
+    else:
+        return fibo(n-1) + fibo(n-2)
+print(fibo(5))
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,51 +1,7 @@
-
-
-
-# def binary_seacrh(arr,target):
-    
-#     low = 0 
-#     high = len(arr) - 1
-    
-#     while low <= high:
-#         mid = ((low + high) //2 )
-        
-#         if arr[mid] == target:
-#             return mid 
-
-#         elif arr[mid] <= target:
-#             low = mid + 1 
-        
-#         else :
-#             high = mid - 1
-            
-#     return -1 
-
-# arr = [1,2,4,5,6,9,12,18,19]
-
-# print(binary_seacrh(arr,12))
-
-
-# # lower bound problem solved leetcode
-
-# class Solution:
-#     def searchInsert(self, nums: List[int], target: int) -> int:
-#         low = 0 
-#         high = len(nums) - 1 
-
-#         while low <= high :
-#             mid = ((low + high) // 2)
-
-#             if nums[mid] == target:
-#                 return mid 
-
-#             elif nums[mid] <= target:
-#                 low = mid + 1 
-                
-#             else :
-#                  high = mid - 1
-                 
-
-#         return low
-        
-        
-#         #  2nd approach of ame question
+def fctorial(n):
+#     # base cond 
+#     if n == 0:
+#         return 1
+#     else:
+#         return n * fctorial(n-1)
+# print(fctorial(5))
