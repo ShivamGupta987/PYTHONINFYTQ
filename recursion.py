c3ba51d6a3bc753a4a39c5776b7131c599160ecb
nikhil <sg804595@gmail.com>
2024-06-27 11:30:31 +0530

    solve 1 to n using recursion

--- recursion.py
+++ recursion.py
@@ -1,14 +1,35 @@
-def fctorial(n):
-    # base cond 
-    if n == 0:
-        return 1
-    else:
-        return n * fctorial(n-1)
-print(fctorial(5))
-def fibo(n):
-    # base cond 
-    if n<=1:
-        return n
-    else:
-        return fibo(n-1) + fibo(n-2)
-print(fibo(5))
+# def fctorial(n):
+#     # base cond 
+#     if n == 0:
+#         return 1
+#     else:
+#         return n * fctorial(n-1)
+# print(fctorial(5))
+# def fibo(n):
+#     # base cond 
+#     if n<=1:
+#         return n
+#     else:
+#         return fibo(n-1) + fibo(n-2)
+# print(fibo(5))
+
+#  print 1 to n using rucursion
+
+def print_no(n):
+    #base cond n>0
+    if n>0:
+        print_no(n-1)
+        print(n)
+print_no(5)
+ 
+print('--------------------------->>>>>>>>>>>>>>>') 
+
+#  print n to 1 using rucursion
+
+def print_no(n):
+    #base cond n>0
+    if n>0:
+        print(n)
+        print_no(n-1)
+        
+print_no(5)
