a6dcbc157f0cf9c27ed2a9b4116dfb7df0f96cc9
nikhil <sg804595@gmail.com>
2024-07-06 12:02:56 +0530

    solve problems on cycle sort

---
+++ problems_on_sorting.py
@@ -0,0 +1,15 @@
+# #missing number leetcode
+# #1 method
+# class Solution:
+#     def missingNumber(self, nums: List[int]) -> int:
+#         n = len(nums)
+
+#         expected_sum = n*(n+1) // 2 
+#         actual_sum = sum(nums)
+
+#         return expected_sum - actual_sum
+        
+        
+#methdo 2 cycle sort
+
+def cycle_Sort()
