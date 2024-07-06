0cae94a8b1564b9bd81497b5754bd88467765f49
nikhil <sg804595@gmail.com>
2024-07-06 12:10:11 +0530

    solve misisng number problem using cycle_sort

--- problems_on_sorting.py
+++ problems_on_sorting.py
@@ -1,15 +1,31 @@
-# #missing number leetcode
-# #1 method
-# class Solution:
-#     def missingNumber(self, nums: List[int]) -> int:
-#         n = len(nums)
+#missing number leetcode
+#1 method
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
 
-#         expected_sum = n*(n+1) // 2 
-#         actual_sum = sum(nums)
+        expected_sum = n*(n+1) // 2 
+        actual_sum = sum(nums)
 
-#         return expected_sum - actual_sum
+        return expected_sum - actual_sum
         
         
 #methdo 2 cycle sort
+# time complexity = O(n)
 
-def cycle_Sort()
+def cycle_Sort(nums):
+    i = 0 
+    n = len(nums)
+    while i < n:
+        correct_index = nums[i]
+        if nums[i]!=nums[correct_index] and nums[i] < n:
+            nums[i],nums[correct_index] = nums[correct_index],nums[i] 
+        else:
+            i+=1 
+            
+    for i in range(n):
+        if nums[i]!=i:
+            return i
+    return n
+            
+            
