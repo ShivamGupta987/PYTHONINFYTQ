7e4b1a434bdf1a6f1b4b4b53c8b2b334a9186e06
nikhil <sg804595@gmail.com>
2024-07-13 23:01:23 +0530

    add

--- composition_relation.py
+++ composition_relation.py
@@ -102,3 +102,311 @@ c.printdetails()
         
         
         
+        
+
+
+class Engine:
+    def enginedetails(self):
+        print("Engine Details:")
+        
+class Tyres:
+    def tyresdetails(self):
+        print("tyres Details:")
+class Doors:
+    def doorsdetails(self):
+        print("Doors Details:")
+        
+        # car ke bina uppare wale ka kuch nhi isliye composition relation
+        # niche jo object hai sb car pe depended hai
+        
+class Car:
+    def __init__(self):
+        self.engine = Engine()
+        self.tyres = Tyres()
+        self.doors = Doors()
+
+    def printdetails(self):
+        self.engine.enginedetails()
+        self.tyres.tyresdetails()
+        
+        
+        self.doors.doorsdetails()
+        
+c = Car()
+
+c.printdetails()
+        # rewritee 
+        
+        
+
+
+class Engine:
+    def enginedetails(self):
+        print("Engine Details:")
+        
+class Tyres:
+    def tyresdetails(self):
+        print("tyres Details:")
+class Doors:
+    def doorsdetails(self):
+        print("Doors Details:")
+        
+        # car ke bina uppare wale ka kuch nhi isliye composition relation
+        # niche jo object hai sb car pe depended hai
+        
+class Car:
+    def __init__(self):
+        self.engine = Engine()
+        self.tyres = Tyres()
+        self.doors = Doors()
+
+    def printdetails(self):
+        self.engine.enginedetails()
+        self.tyres.tyresdetails()
+        
+        
+        self.doors.doorsdetails()
+        
+c = Car()
+
+c.printdetails()
+        
+        
+
+
+class Engine:
+    def enginedetails(self):
+        print("Engine Details:")
+        
+class Tyres:
+    def tyresdetails(self):
+        print("tyres Details:")
+class Doors:
+    def doorsdetails(self):
+        print("Doors Details:")
+        
+        # car ke bina uppare wale ka kuch nhi isliye composition relation
+        # niche jo object hai sb car pe depended hai
+        
+class Car:
+    def __init__(self):
+        self.engine = Engine()
+        self.tyres = Tyres()
+        self.doors = Doors()
+
+    def printdetails(self):
+        self.engine.enginedetails()
+        self.tyres.tyresdetails()
+        
+        
+        self.doors.doorsdetails()
+        
+c = Car()
+
+c.printdetails()
+
+
+
+class Engine:
+    def enginedetails(self):
+        print("Engine Details:")
+        
+class Tyres:
+    def tyresdetails(self):
+        print("tyres Details:")
+class Doors:
+    def doorsdetails(self):
+        print("Doors Details:")
+        
+        # car ke bina uppare wale ka kuch nhi isliye composition relation
+        # niche jo object hai sb car pe depended hai
+        
+class Car:
+    def __init__(self):
+        self.engine = Engine()
+        self.tyres = Tyres()
+        self.doors = Doors()
+
+    def printdetails(self):
+        self.engine.enginedetails()
+        self.tyres.tyresdetails()
+        
+        
+        self.doors.doorsdetails()
+        
+c = Car()
+
+c.printdetails()
+        # rewritee 
+        
+        
+
+
+class Engine:
+    def enginedetails(self):
+        print("Engine Details:")
+        
+class Tyres:
+    def tyresdetails(self):
+        print("tyres Details:")
+class Doors:
+    def doorsdetails(self):
+        print("Doors Details:")
+        
+        # car ke bina uppare wale ka kuch nhi isliye composition relation
+        # niche jo object hai sb car pe depended hai
+        
+class Car:
+    def __init__(self):
+        self.engine = Engine()
+        self.tyres = Tyres()
+        self.doors = Doors()
+
+    def printdetails(self):
+        self.engine.enginedetails()
+        self.tyres.tyresdetails()
+        
+        
+        self.doors.doorsdetails()
+        
+c = Car()
+
+c.printdetails()
+        
+        
+
+
+class Engine:
+    def enginedetails(self):
+        print("Engine Details:")
+        
+class Tyres:
+    def tyresdetails(self):
+        print("tyres Details:")
+class Doors:
+    def doorsdetails(self):
+        print("Doors Details:")
+        
+        # car ke bina uppare wale ka kuch nhi isliye composition relation
+        # niche jo object hai sb car pe depended hai
+        
+class Car:
+    def __init__(self):
+        self.engine = Engine()
+        self.tyres = Tyres()
+        self.doors = Doors()
+
+    def printdetails(self):
+        self.engine.enginedetails()
+        self.tyres.tyresdetails()
+        
+        
+        self.doors.doorsdetails()
+        
+c = Car()
+
+c.printdetails()
+
+
+
+
+
+class Engine:
+    def enginedetails(self):
+        print("Engine Details:")
+        
+class Tyres:
+    def tyresdetails(self):
+        print("tyres Details:")
+class Doors:
+    def doorsdetails(self):
+        print("Doors Details:")
+        
+        # car ke bina uppare wale ka kuch nhi isliye composition relation
+        # niche jo object hai sb car pe depended hai
+        
+class Car:
+    def __init__(self):
+        self.engine = Engine()
+        self.tyres = Tyres()
+        self.doors = Doors()
+
+    def printdetails(self):
+        self.engine.enginedetails()
+        self.tyres.tyresdetails()
+        
+        
+        self.doors.doorsdetails()
+        
+c = Car()
+
+c.printdetails()
+        # rewritee 
+        
+        
+
+
+class Engine:
+    def enginedetails(self):
+        print("Engine Details:")
+        
+class Tyres:
+    def tyresdetails(self):
+        print("tyres Details:")
+class Doors:
+    def doorsdetails(self):
+        print("Doors Details:")
+        
+        # car ke bina uppare wale ka kuch nhi isliye composition relation
+        # niche jo object hai sb car pe depended hai
+        
+class Car:
+    def __init__(self):
+        self.engine = Engine()
+        self.tyres = Tyres()
+        self.doors = Doors()
+
+    def printdetails(self):
+        self.engine.enginedetails()
+        self.tyres.tyresdetails()
+        
+        
+        self.doors.doorsdetails()
+        
+c = Car()
+
+c.printdetails()
+        
+        
+
+
+class Engine:
+    def enginedetails(self):
+        print("Engine Details:")
+        
+class Tyres:
+    def tyresdetails(self):
+        print("tyres Details:")
+class Doors:
+    def doorsdetails(self):
+        print("Doors Details:")
+        
+        # car ke bina uppare wale ka kuch nhi isliye composition relation
+        # niche jo object hai sb car pe depended hai
+        
+class Car:
+    def __init__(self):
+        self.engine = Engine()
+        self.tyres = Tyres()
+        self.doors = Doors()
+
+    def printdetails(self):
+        self.engine.enginedetails()
+        self.tyres.tyresdetails()
+        
+        
+        self.doors.doorsdetails()
+        
+c = Car()
+
+c.printdetails()
