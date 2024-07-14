00aa1c82904f1a8c10d02ad5703dd8f836d46564
nikhil <sg804595@gmail.com>
2024-07-14 19:34:10 +0530

    some things deleetd

--- problems_on_sorting.py
+++ problems_on_sorting.py
@@ -50,3 +50,25 @@ class Solution:
 
         missing_no = all_nums - num_set 
         return list(missing_no)
+    
+    
+#  leetcode setmismatch problen 645 
+
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        nums = sorted(nums)
+        duplicate = -1 
+        n = len(nums)
+
+        expected_sum = n*(n+1) // 2
+
+        for i in range(1,n):
+            if nums[i] == nums[i-1]:
+                duplicate = nums[i]
+                break
+        actual_sum = sum(nums) - duplicate 
+
+        missing = expected_sum - actual_sum 
+
+        return [duplicate,missing]
+        
