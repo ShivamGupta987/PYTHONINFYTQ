ad72d19837cd6e36fbebe1dd6aefd79a078ab5ce
nikhil <sg804595@gmail.com>
2024-07-20 11:20:02 +0530

    insert end begining position completed

--- insertion_linked_list.py
+++ insertion_linked_list.py
@@ -12,6 +12,41 @@ class LinkedList:
         new_node.next = self.head
         self.head = new_node
     
+    def insert_at_end(self, data):
+        new_node = Node(data)
+        
+        if self.head is None:
+            self.head = new_node
+            return
+        
+        current = self.head
+        while current.next:
+            current = current.next
+        
+        current.next = new_node
+    
+    def insert_at_pos(self, data, position):
+        new_node = Node(data)
+        
+        if position == 1:
+            new_node.next = self.head
+            self.head = new_node
+            return
+        
+        current = self.head
+        for i in range(1, position - 1):
+            if current is None:
+                print("Invalid position")
+                return self.head
+            current = current.next
+        
+        if current is None:
+            print("Invalid position")
+            return self.head
+        
+        new_node.next = current.next
+        current.next = new_node
+    
     def display(self):
         current = self.head
         while current:
@@ -25,26 +60,7 @@ if __name__ == '__main__':
     ll.insert_at_beginning(3)
     ll.insert_at_beginning(7)
     ll.insert_at_beginning(1)
+    ll.insert_at_end(100)
+    ll.insert_at_pos(50, 3)
     
     ll.display()
-
-
-
-#  insert at end
-
-def insert_at_end(head,value):
-    new_node = Node(value)
-    
-    if head is None:
-        return new_node 
-    
-    current = head 
-    while current.next is not None:
-        current = current.next
-        
-    current.next = new_node 
-    return head 
-
-head = insert_at_end(head,100)
-
-        '
