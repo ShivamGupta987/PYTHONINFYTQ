d743bc8b1330a349bc20ab89f68879a20c36b72a
nikhil <sg804595@gmail.com>
2024-07-28 20:40:10 +0530

    add

--- encapsultaion.py
+++ encapsultaion.py
@@ -24,4 +24,82 @@ print(per.getName())
         
         
     
+
+class Person:
+    def __init__(self,name,car):
+        # private class foiedld
+        self.__name = name
+        self.__car = car
+
+    # geeter and setter methpod to interaction with the private fields 
+    
+    def getName(self):
+        return self.__name
+    def setName(self,name):
+        self.__name = name 
+        
+    def getCar(self):
+        return self.__car
+    def setCar(self,car):
+        self.__car = car
+        
+per = Person('shivam',89)
+print(per.getName())
+per.setName('shivamgupta')
+print(per.getName())
+        
+        
+    
+
+class Person:
+    def __init__(self,name,car):
+        # private class foiedld
+        self.__name = name
+        self.__car = car
+
+    # geeter and setter methpod to interaction with the private fields 
+    
+    def getName(self):
+        return self.__name
+    def setName(self,name):
+        self.__name = name 
+        
+    def getCar(self):
+        return self.__car
+    def setCar(self,car):
+        self.__car = car
+        
+per = Person('shivam',89)
+print(per.getName())
+per.setName('shivamgupta')
+print(per.getName())
+        
+        
+    
+
+class Person:
+    def __init__(self,name,car):
+        # private class foiedld
+        self.__name = name
+        self.__car = car
+
+    # geeter and setter methpod to interaction with the private fields 
+    
+    def getName(self):
+        return self.__name
+    def setName(self,name):
+        self.__name = name 
+        
+    def getCar(self):
+        return self.__car
+    def setCar(self,car):
+        self.__car = car
+        
+per = Person('shivam',89)
+print(per.getName())
+per.setName('shivamgupta')
+print(per.getName())
+        
+        
+    
         
