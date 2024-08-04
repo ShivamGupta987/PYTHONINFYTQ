81f8de6caf89f11e7b107251129c375e2de537cf
nikhil <sg804595@gmail.com>
2024-08-04 22:29:33 +0530

    solve 503 problem on leetcode

--- Stack_problems.py
+++ Stack_problems.py
@@ -169,3 +169,27 @@ class Solution:
 sol = Solution()
 print(sol.isValid("{()}"))  
 print(sol.isValid("{[}"))  
+
+# leetcode 503
+
+
+from typing import List
+
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        res = [-1] * n  
+        stack = []  
+
+        for i in range(2 * n):
+            num = nums[i % n]  
+            
+
+            while stack and nums[stack[-1]] < num:
+                index = stack.pop()  
+                res[index] = num  
+            
+         
+            stack.append(i % n)
+        
+        return res
