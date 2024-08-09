70b96e706de8a793dec9993c314b9de7c01d074a
nikhil <sg804595@gmail.com>
2024-08-09 23:00:22 +0530

    add

--- sldingwindow-twopointer.py
+++ sldingwindow-twopointer.py
@@ -110,3 +110,22 @@ def max_Sum(arr, k):
 arr = [6, 2, 3, 4, 7, 2, 1, 7, 1]
 k = 4
 print(max_Sum(arr, k)) 
+
+# 209 leetcode solved
+
+
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        left = 0
+        current_sum = 0
+        min_length = float('inf')
+        
+        for right in range(len(nums)):
+            current_sum += nums[right]
+            
+            while current_sum >= target:
+                min_length = min(min_length, right - left + 1)
+                current_sum -= nums[left]
+                left += 1
+        
+        return min_length if min_length != float('inf') else 0
