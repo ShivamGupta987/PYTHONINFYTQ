6a733a87259e6d849dbce7c5b67bc907345e0268
nikhil <sg804595@gmail.com>
2024-08-15 23:46:48 +0530

    com

--- python2.py
+++ python2.py
@@ -44,3 +44,267 @@ def generate_ticket(airline, source, destination, no_of_passengers):
 print(generate_ticket("AI", "Bangalore", "London", 10))
 print(generate_ticket("BA", "Australia", "France", 2))
 # rewrire
+
+
+#lex_auth_01269438070259712046
+
+def count_names(name_list):
+    count1=0
+    count2=0
+    
+    #start writing your code here
+    
+    
+    for name in name_list:
+        if len(name)>=3 and name[-2:] == 'at':
+            count1+=1
+        
+        if 'at' in name:
+            count2+=1
+            
+            
+    print("_at -> ",count1)
+    print("%at% -> ",count2)
+ 
+    #Populate the variables: count1 and count2
+
+    # Use the below given print statements to display the output
+    # Also, do not modify them for verification to work
+    # print("_at -> ",count1)
+    # print("%at% -> ",count2)
+
+
+#Provide different names in the list and test your program
+name_list=["Hat","Cat","rabbit","matter"]
+count_names(name_list)
+
+
+#lex_auth_01269438070259712046
+
+def count_names(name_list):
+    count1=0
+    count2=0
+    
+    #start writing your code here
+    
+    
+    for name in name_list:
+        if len(name)>=3 and name[-2:] == 'at':
+            count1+=1
+        
+        if 'at' in name:
+            count2+=1
+            
+            
+    print("_at -> ",count1)
+    print("%at% -> ",count2)
+ 
+    #Populate the variables: count1 and count2
+
+    # Use the below given print statements to display the output
+    # Also, do not modify them for verification to work
+    # print("_at -> ",count1)
+    # print("%at% -> ",count2)
+
+
+#Provide different names in the list and test your program
+name_list=["Hat","Cat","rabbit","matter"]
+count_names(name_list)
+
+#lex_auth_01269438070259712046
+
+def count_names(name_list):
+    count1=0
+    count2=0
+    
+    #start writing your code here
+    
+    
+    for name in name_list:
+        if len(name)>=3 and name[-2:] == 'at':
+            count1+=1
+        
+        if 'at' in name:
+            count2+=1
+            
+            
+    print("_at -> ",count1)
+    print("%at% -> ",count2)
+ 
+    #Populate the variables: count1 and count2
+
+    # Use the below given print statements to display the output
+    # Also, do not modify them for verification to work
+    # print("_at -> ",count1)
+    # print("%at% -> ",count2)
+
+
+#Provide different names in the list and test your program
+name_list=["Hat","Cat","rabbit","matter"]
+count_names(name_list)
+
+
+#lex_auth_01269438070259712046
+
+def count_names(name_list):
+    count1=0
+    count2=0
+    
+    #start writing your code here
+    
+    
+    for name in name_list:
+        if len(name)>=3 and name[-2:] == 'at':
+            count1+=1
+        
+        if 'at' in name:
+            count2+=1
+            
+            
+    print("_at -> ",count1)
+    print("%at% -> ",count2)
+ 
+    #Populate the variables: count1 and count2
+
+    # Use the below given print statements to display the output
+    # Also, do not modify them for verification to work
+    # print("_at -> ",count1)
+    # print("%at% -> ",count2)
+
+
+#Provide different names in the list and test your program
+name_list=["Hat","Cat","rabbit","matter"]
+count_names(name_list)
+
+
+
+
+#lex_auth_01269438070259712046
+
+def count_names(name_list):
+    count1=0
+    count2=0
+    
+    #start writing your code here
+    
+    
+    for name in name_list:
+        if len(name)>=3 and name[-2:] == 'at':
+            count1+=1
+        
+        if 'at' in name:
+            count2+=1
+            
+            
+    print("_at -> ",count1)
+    print("%at% -> ",count2)
+ 
+    #Populate the variables: count1 and count2
+
+    # Use the below given print statements to display the output
+    # Also, do not modify them for verification to work
+    # print("_at -> ",count1)
+    # print("%at% -> ",count2)
+
+
+#Provide different names in the list and test your program
+name_list=["Hat","Cat","rabbit","matter"]
+count_names(name_list)
+
+
+#lex_auth_01269438070259712046
+
+def count_names(name_list):
+    count1=0
+    count2=0
+    
+    #start writing your code here
+    
+    
+    for name in name_list:
+        if len(name)>=3 and name[-2:] == 'at':
+            count1+=1
+        
+        if 'at' in name:
+            count2+=1
+            
+            
+    print("_at -> ",count1)
+    print("%at% -> ",count2)
+ 
+    #Populate the variables: count1 and count2
+
+    # Use the below given print statements to display the output
+    # Also, do not modify them for verification to work
+    # print("_at -> ",count1)
+    # print("%at% -> ",count2)
+
+
+#Provide different names in the list and test your program
+name_list=["Hat","Cat","rabbit","matter"]
+count_names(name_list)
+
+#lex_auth_01269438070259712046
+
+def count_names(name_list):
+    count1=0
+    count2=0
+    
+    #start writing your code here
+    
+    
+    for name in name_list:
+        if len(name)>=3 and name[-2:] == 'at':
+            count1+=1
+        
+        if 'at' in name:
+            count2+=1
+            
+            
+    print("_at -> ",count1)
+    print("%at% -> ",count2)
+ 
+    #Populate the variables: count1 and count2
+
+    # Use the below given print statements to display the output
+    # Also, do not modify them for verification to work
+    # print("_at -> ",count1)
+    # print("%at% -> ",count2)
+
+
+#Provide different names in the list and test your program
+name_list=["Hat","Cat","rabbit","matter"]
+count_names(name_list)
+
+
+#lex_auth_01269438070259712046
+
+def count_names(name_list):
+    count1=0
+    count2=0
+    
+    #start writing your code here
+    
+    
+    for name in name_list:
+        if len(name)>=3 and name[-2:] == 'at':
+            count1+=1
+        
+        if 'at' in name:
+            count2+=1
+            
+            
+    print("_at -> ",count1)
+    print("%at% -> ",count2)
+ 
+    #Populate the variables: count1 and count2
+
+    # Use the below given print statements to display the output
+    # Also, do not modify them for verification to work
+    # print("_at -> ",count1)
+    # print("%at% -> ",count2)
+
+
+#Provide different names in the list and test your program
+name_list=["Hat","Cat","rabbit","matter"]
+count_names(name_list)
